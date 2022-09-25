c17a8af874df3b7cfa5b1f956658613fd7387bf5
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-09-26 00:27:14 +0900

    style: Spotless formatting. (#2)

--- src/commonMain/kotlin/com/autonomouslogic/isaac/Isaac.kt
+++ src/commonMain/kotlin/com/autonomouslogic/isaac/Isaac.kt
@@ -26,62 +26,62 @@ class Isaac {
 		// cc just gets incremented once per 256 results
 		cc = cc + 1
 		// then combined with bb
-		bb = bb + cc;
+		bb = bb + cc
 
 		for (i in 0..255) {
-			x = mm[i];
+			x = mm[i]
 
-			when (i%4) {
+			when (i % 4) {
 				0 -> aa = aa xor (aa shl 13)
 				1 -> aa = aa xor (aa ushr 6)
 				2 -> aa = aa xor (aa shl 2)
 				3 -> aa = aa xor (aa ushr 16)
 			}
 			aa = mm[(i + 128) % 256] + aa
-			y  = mm[(x ushr 2).mod(256)] + aa + bb
+			y = mm[(x ushr 2).mod(256)] + aa + bb
 			mm[i] = y
 			bb = mm[(y ushr 10).mod(256)] + x
 			randrsl[i] = bb
 
-			/* Note that bits 2..9 are chosen from x but 10..17 are chosen
-			   from y.  The only important thing here is that 2..9 and 10..17
-			   don't overlap.  2..9 and 10..17 were then chosen for speed in
-			   the optimized version (rand.c) */
-			/* See http://burtleburtle.net/bob/rand/isaac.html
-			   for further explanations and analysis. */
+	/* Note that bits 2..9 are chosen from x but 10..17 are chosen
+	from y.  The only important thing here is that 2..9 and 10..17
+	don't overlap.  2..9 and 10..17 were then chosen for speed in
+	the optimized version (rand.c) */
+	/* See http://burtleburtle.net/bob/rand/isaac.html
+	for further explanations and analysis. */
 		}
 	}
 
 	private fun mix(s: IntArray) {
-		s[0] = s[0] xor ( s[1] shl 11 )
+		s[0] = s[0] xor (s[1] shl 11)
 		s[3] += s[0]
 		s[1] += s[2]
 
-		s[1] = s[1] xor ( s[2] ushr 2 )
+		s[1] = s[1] xor (s[2] ushr 2)
 		s[4] += s[1]
 		s[2] += s[3]
 
-		s[2] = s[2] xor ( s[3] shl 8 )
+		s[2] = s[2] xor (s[3] shl 8)
 		s[5] += s[2]
 		s[3] += s[4]
 
-		s[3] = s[3] xor ( s[4] ushr 16 )
+		s[3] = s[3] xor (s[4] ushr 16)
 		s[6] += s[3]
 		s[4] += s[5]
 
-		s[4] = s[4] xor ( s[5] shl 10 )
+		s[4] = s[4] xor (s[5] shl 10)
 		s[7] += s[4]
 		s[5] += s[6]
 
-		s[5] = s[5] xor ( s[6] ushr 4 )
+		s[5] = s[5] xor (s[6] ushr 4)
 		s[0] += s[5]
 		s[6] += s[7]
 
-		s[6] = s[6] xor ( s[7] shl 8 )
+		s[6] = s[6] xor (s[7] shl 8)
 		s[1] += s[6]
 		s[7] += s[0]
 
-		s[7] = s[7] xor ( s[0] ushr 9 )
+		s[7] = s[7] xor (s[0] ushr 9)
 		s[2] += s[7]
 		s[0] += s[1]
 	}
@@ -98,49 +98,49 @@ class Isaac {
 		}
 
 		// fill in mm[] with messy stuff
-		for (i in 0 .. 255 step 8) {
+		for (i in 0..255 step 8) {
 			s[0] += randrsl[i]
-			s[1] += randrsl[i+1]
-			s[2] += randrsl[i+2]
-			s[3] += randrsl[i+3]
-			s[4] += randrsl[i+4]
-			s[5] += randrsl[i+5]
-			s[6] += randrsl[i+6]
-			s[7] += randrsl[i+7]
+			s[1] += randrsl[i + 1]
+			s[2] += randrsl[i + 2]
+			s[3] += randrsl[i + 3]
+			s[4] += randrsl[i + 4]
+			s[5] += randrsl[i + 5]
+			s[6] += randrsl[i + 6]
+			s[7] += randrsl[i + 7]
 
 			mix(s)
 
-			mm[i  ] = s[0]
-			mm[i+1] = s[1]
-			mm[i+2] = s[2]
-			mm[i+3] = s[3]
-			mm[i+4] = s[4]
-			mm[i+5] = s[5]
-			mm[i+6] = s[6]
-			mm[i+7] = s[7]
+			mm[i] = s[0]
+			mm[i + 1] = s[1]
+			mm[i + 2] = s[2]
+			mm[i + 3] = s[3]
+			mm[i + 4] = s[4]
+			mm[i + 5] = s[5]
+			mm[i + 6] = s[6]
+			mm[i + 7] = s[7]
 		}
 
 		// do a second pass to make all of the seed affect all of mm
-		for (i in 0 .. 255 step 8) {
-			s[0] += mm[i  ]
-			s[1] += mm[i+1]
-			s[2] += mm[i+2]
-			s[3] += mm[i+3]
-			s[4] += mm[i+4]
-			s[5] += mm[i+5]
-			s[6] += mm[i+6]
-			s[7] += mm[i+7]
+		for (i in 0..255 step 8) {
+			s[0] += mm[i]
+			s[1] += mm[i + 1]
+			s[2] += mm[i + 2]
+			s[3] += mm[i + 3]
+			s[4] += mm[i + 4]
+			s[5] += mm[i + 5]
+			s[6] += mm[i + 6]
+			s[7] += mm[i + 7]
 
-			mix(s);
+			mix(s)
 
-			mm[i  ] = s[0]
-			mm[i+1] = s[1]
-			mm[i+2] = s[2]
-			mm[i+3] = s[3]
-			mm[i+4] = s[4]
-			mm[i+5] = s[5]
-			mm[i+6] = s[6]
-			mm[i+7] = s[7]
+			mm[i] = s[0]
+			mm[i + 1] = s[1]
+			mm[i + 2] = s[2]
+			mm[i + 3] = s[3]
+			mm[i + 4] = s[4]
+			mm[i + 5] = s[5]
+			mm[i + 6] = s[6]
+			mm[i + 7] = s[7]
 		}
 
 		// fill in the first set of results
@@ -159,7 +159,7 @@ class Isaac {
 			}
 			val newSeed = IntArray(SEED_SIZE)
 			val n = minOf(seed.size, SEED_SIZE)
-			for (i in 0..n-1) {
+			for (i in 0..n - 1) {
 				newSeed[i] = seed[i]
 			}
 			return newSeed
